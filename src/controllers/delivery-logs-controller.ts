1f603ef6c666e85ea8f3a3a933138b290a0b07f8
Ryan Mazali <ryanmazali17@gmail.com>
2026-05-04 15:31:35 -0300

    fix: show delivery not found case not exist

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -62,6 +62,10 @@ class DeliveryLogsController{
             },
         })
 
+        if(!delivery) {
+            return response.status(404).json({ message: "Delivery not found" })
+        }
+
         if(
             request.user?.role === "customer" && 
             request.user.id !== delivery?.userId
