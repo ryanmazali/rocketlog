21b1ba550962fcc2c0ee95f1c6be1bb0b999b03c
Ryan Mazali <ryanmazali17@gmail.com>
2026-05-04 14:29:06 -0300

    chore: adjusting package.json for the deploy

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,8 @@ import { z } from "zod"
 
 const envSchema = z.object({
     DATABASE_URL: z.string().url(),
-    JWT_SECRET: z.string().min(6)
+    JWT_SECRET: z.string().min(6),
+    PORT: z.coerce.number().default(3333)
 })
 
 export const env = envSchema.parse(process.env)
